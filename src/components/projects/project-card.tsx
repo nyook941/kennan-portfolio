1a64a80489f90f6068f1879f46b2dc51a1b50694
nyook941 <kennan.k.wu@gmail.com>
2024-02-01 15:49:17 -0600

    work on projects section

--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -2,14 +2,14 @@ import "./projects.css";
 
 export default function ProjectCard() {
   return (
-    <div className="Project-Card-Container">
-      <div className="Project-Card-Box">
-        <img
-          src="/images/motherboard_id_thumbnail.png"
-          className="Project-Card-Image"
-        ></img>
-        <span>Motherboard Identification</span>
-      </div>
+    <div className="Projects-Card">
+      <img src="/images/motherboard_id_thumbnail.png"></img>
+      <h1>Motherboard Inspector</h1>
+      <p>
+        Winner of the Summer 2023 UTDesign Expo, the motherboard inspector uses
+        AI/ML to automate the identification and diagnoses of faulty
+        motherboards.
+      </p>
     </div>
   );
 }
